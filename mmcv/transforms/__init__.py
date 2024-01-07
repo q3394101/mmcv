7960d1e9a84364e3c9e81c0c5dc67849887ff653
tripleMu <gpu@163.com>
2024-01-07 15:19:27 +0800

    support load from npy file

--- mmcv/transforms/__init__.py
+++ mmcv/transforms/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import BaseTransform
 from .builder import TRANSFORMS
-from .loading import LoadAnnotations, LoadImageFromFile
+from .loading import LoadAnnotations, LoadImageFromFile, LoadNpyFromFile
 from .processing import (CenterCrop, MultiScaleFlipAug, Normalize, Pad,
                          RandomChoiceResize, RandomFlip, RandomGrayscale,
                          RandomResize, Resize, TestTimeAug)
@@ -13,18 +13,19 @@ try:
 except ImportError:
     __all__ = [
         'BaseTransform', 'TRANSFORMS', 'TransformBroadcaster', 'Compose',
-        'RandomChoice', 'KeyMapper', 'LoadImageFromFile', 'LoadAnnotations',
-        'Normalize', 'Resize', 'Pad', 'RandomFlip', 'RandomChoiceResize',
-        'CenterCrop', 'RandomGrayscale', 'MultiScaleFlipAug', 'RandomResize',
-        'RandomApply', 'TestTimeAug'
+        'RandomChoice', 'KeyMapper', 'LoadImageFromFile', 'LoadNpyFromFile',
+        'LoadAnnotations', 'Normalize', 'Resize', 'Pad', 'RandomFlip',
+        'RandomChoiceResize', 'CenterCrop', 'RandomGrayscale',
+        'MultiScaleFlipAug', 'RandomResize', 'RandomApply', 'TestTimeAug'
     ]
 else:
     from .formatting import ImageToTensor, ToTensor, to_tensor
 
     __all__ = [
         'BaseTransform', 'TRANSFORMS', 'TransformBroadcaster', 'Compose',
-        'RandomChoice', 'KeyMapper', 'LoadImageFromFile', 'LoadAnnotations',
-        'Normalize', 'Resize', 'Pad', 'ToTensor', 'to_tensor', 'ImageToTensor',
-        'RandomFlip', 'RandomChoiceResize', 'CenterCrop', 'RandomGrayscale',
-        'MultiScaleFlipAug', 'RandomResize', 'RandomApply', 'TestTimeAug'
+        'RandomChoice', 'KeyMapper', 'LoadImageFromFile', 'LoadNpyFromFile',
+        'LoadAnnotations', 'Normalize', 'Resize', 'Pad', 'ToTensor',
+        'to_tensor', 'ImageToTensor', 'RandomFlip', 'RandomChoiceResize',
+        'CenterCrop', 'RandomGrayscale', 'MultiScaleFlipAug', 'RandomResize',
+        'RandomApply', 'TestTimeAug'
     ]
